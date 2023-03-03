a1246d826bf04f6626ee5b58674e1448a3e14e48
DiegoArion <70103662+DiegoArion@users.noreply.github.com>
2023-03-03 10:33:53 -0600

    Delete src directory

--- src/main/java/com/example/ecommerce/repository/ICategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/example/ecommerce/repository/IStateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.model.State;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IStateRepository extends JpaRepository<State, Long> {
-
-}
